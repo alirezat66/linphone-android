4dd641b514a4fecee939b5a7918ee678f0231974
Sepehr Tabeian <sepehrtabeian@gmail.com>
2022-09-09 23:43:09 +0430

    feat : complete login api service and push notification service

--- app/src/main/java/org/linphone/services/LoginApiService.kt
+++ app/src/main/java/org/linphone/services/LoginApiService.kt
@@ -2,15 +2,14 @@ package org.linphone.services
 
 import com.google.gson.JsonObject
 import org.linphone.model.response.login.LoginModel
+import org.linphone.model.response.loginStatus.LoginStatusModel
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface LoginApiService {
     @POST("/Login")
-//    @POST("/LoginStatus")
-//    @POST("/PushNotification")
     fun login(@Body body: JsonObject?): Call<LoginModel?>?
-    //    @POST("/LoginStatus")
-//    fun loginStatus(@Body body: JsonObject?) Call<>
+        @POST("/LoginStatus")
+    fun loginStatus(@Body body: JsonObject?) : Call<LoginStatusModel>
 }
--- app/src/main/java/org/linphone/services/PushNotificationApiService.kt
+++ app/src/main/java/org/linphone/services/PushNotificationApiService.kt
@@ -1,6 +1,12 @@
 package org.linphone.services
 
+import com.google.gson.JsonObject
+import org.linphone.model.response.pushNotification.PushNotificationModel
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface PushNotificationApiService {
-    //    @POST("/PushNotification")
-//    fun loginStatus(@Body body: JsonObject?) Call<>
+        @POST("/PushNotification")
+    fun pushNotificationRequest(@Body body: JsonObject?) : Call<PushNotificationModel>
 }
